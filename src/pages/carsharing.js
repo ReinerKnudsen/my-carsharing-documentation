503992077f83790970965c676093ab88c02ea95a
Reiner Knudsen <reinerknudsen@me.com>
2025-12-23 10:42:28 +0000

    initial commit

---
+++ src/pages/carsharing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>Carsharing is the best</h1>
+      <p>My first documentation page</p>
+    </Layout>
+  );
+}
